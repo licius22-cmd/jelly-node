8471ea10d250fd085e6055516b8e6c3c0cd95fc2
licius22-cmd <licius22@gmail.com>
2026-03-09 18:16:58 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [], // <-- É exatamente dessa linha vazia que a Cloudflare precisa
-  build: {
-    outDir: 'dist',
-  }
-});
